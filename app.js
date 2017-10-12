d88db57f2065f56397612837b5322e3427ffea93
Diego Augusto <daugustomoura@gmail.com>
2017-10-12 12:10:01 -0300

    finaliza projeto

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/get-file-size', upload.single('file'), function (req, res){
+    res.json({size : req.file.size });
 });
 
 app.listen(3000);
